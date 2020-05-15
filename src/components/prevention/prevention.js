1fd155c7d29e9ec73ffde49291985b5d0c374c12
PraveenGNair <prgnr173@gmail.com>
2020-05-15 14:29:00 +1000

    Added some info.

--- src/components/prevention/prevention.js
+++ src/components/prevention/prevention.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import awareness from '../../images/awareness.jpg';
 import awareness2 from '../../images/awareness-2.jpg';
 import awareness3 from '../../images/awareness-3.png';
-
+import { Alert } from '@material-ui/lab';
 export default function GridCard() {
   return (
     <Grid container justify="center">
+      <Grid item xs={6}>
+        <Alert icon={false} severity="info" style={{ display: "block", marginTop: 30 }} >
+          <Typography align="center">
+            <strong>Be Safe, Stay Safe.</strong><br></br>
+            <i>"Optimism is the faith that leads to achievement."</i>
+          </Typography>
+        </Alert>
+      </Grid>
+
+
       <Grid item>
         <img src={awareness} alt="preventive measure 1" style={{ width: '80%', marginTop: 30 }} />
       </Grid>
