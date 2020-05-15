8cd4e65fbf319497fe11da113f68ff69ad61422e
PraveenGNair <prgnr173@gmail.com>
2020-05-15 12:25:07 +1000

    Updates

---
+++ precache-manifest.90e9299fc87666cb124cf328fe0dee76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41895b3193902aca3d2aaf59e62d0433",
+    "url": "/covid19-all-tracker-app/index.html"
+  },
+  {
+    "revision": "a90cd49b4b64a21b6326",
+    "url": "/covid19-all-tracker-app/static/css/main.af3409ed.chunk.css"
+  },
+  {
+    "revision": "d73d20e642bf46d5234c",
+    "url": "/covid19-all-tracker-app/static/js/2.19b0ef5d.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-all-tracker-app/static/js/2.19b0ef5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90cd49b4b64a21b6326",
+    "url": "/covid19-all-tracker-app/static/js/main.44acd2d9.chunk.js"
+  },
+  {
+    "revision": "a037232380b90a863115",
+    "url": "/covid19-all-tracker-app/static/js/runtime-main.01870786.js"
+  },
+  {
+    "revision": "71c5c79df8f2482e5fc456edf1c5a7d9",
+    "url": "/covid19-all-tracker-app/static/media/awareness-2.71c5c79d.jpg"
+  },
+  {
+    "revision": "1458a4732278312db80ca6d75e2b2a6f",
+    "url": "/covid19-all-tracker-app/static/media/awareness-3.1458a473.png"
+  },
+  {
+    "revision": "790524f5820a60f6cab8b757aa58b5a2",
+    "url": "/covid19-all-tracker-app/static/media/awareness.790524f5.jpg"
+  },
+  {
+    "revision": "2e5fd58c551b56ff5b0b6989157e3581",
+    "url": "/covid19-all-tracker-app/static/media/logo.2e5fd58c.png"
+  }
+]);
